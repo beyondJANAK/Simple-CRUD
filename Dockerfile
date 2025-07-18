e8b70f9b971a608ef9c2f8c1ed9eed61f2a9d352
Janak <janakbhat01@gmail.com>
2025-07-18 22:51:03 +0545

    dockerize the application

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:17
 WORKDIR /app
-COPY target/soalteeloyalty-0.0.1-SNAPSHOT.jar /app/app.jar
-ENV JAVA_OPTS="-Xms2g -Xmx4g"
-ENV TZ Asia/Kathmandu
+
+COPY target/*.jar /app/app.jar
 EXPOSE 8090
-ENTRYPOINT exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app/app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/CRUD/controller/EmployeeController.java
+++ src/main/java/com/example/CRUD/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import com.example.CRUD.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
+
+    @GetMapping("/all")
+    public String getAllEmployees() {
+        return "List of all employees";
+    }
+
     @PostMapping("/add")
     public ResponseEntity<Employee> addEmployee(@RequestBody EmployeeDto employeeDto) {
         return new ResponseEntity<>(employeeService.saveEmployee(employeeDto), HttpStatus.CREATED);
